8aba65fe147d64130539ffb8bcdca77c446b7176
Curve <fynnbwdt@gmail.com>
2023-03-24 23:48:13 +0100

    refactor: replace argparse with cli11

--- include/parser.hpp
+++ include/parser.hpp
@@ -1,8 +1,29 @@
 #pragma once
+#include <vector>
+#include <string>
+#include <cstdint>
 #include <optional>
-#include <argparse/argparse.hpp>
+#include <filesystem>
 
 namespace awmtt
 {
-    std::optional<argparse::ArgumentParser> parse(int argc, char **args);
+    struct settings
+    {
+        std::string xephyr;
+        std::string awesome;
+
+        std::string size;
+        std::optional<std::size_t> display;
+
+        std::filesystem::path config;
+
+        std::vector<std::string> xephyr_args;
+        std::vector<std::string> awesome_args;
+
+        bool reload;
+        bool recursive;
+        std::filesystem::path watch;
+    };
+
+    std::optional<settings> parse(int argc, char **args);
 } // namespace awmtt
--- src/parser.cpp
+++ src/parser.cpp
@@ -1,50 +1,81 @@
 #include "parser.hpp"
+
+#include "utils.hpp"
 #include "logger.hpp"
 #include "constants.hpp"
 
 #include <regex>
+#include <CLI/CLI.hpp>
 
 namespace awmtt
 {
-    std::optional<argparse::ArgumentParser> parse(int argc, char **args)
+    std::optional<settings> parse(int argc, char **args)
     {
-        argparse::ArgumentParser parser(std::string{awmtt::name}, std::string{awmtt::version});
+        CLI::App parser{fmt::format("{} - v{}", awmtt::name, awmtt::version)};
+        parser.get_formatter()->column_width(60);
 
-        // Binaries
-        parser.add_argument("--xephyr").help("Location of the xephyr binary").default_value("Xephyr").metavar("PATH");
-        parser.add_argument("--awesome").help("Location of the awesome binary").default_value("awesome").metavar("PATH");
+        settings rtn{};
 
-        // Xephyr Parameters
-        parser.add_argument("--display").help("Display to use").metavar("NUMBER").scan<'u', std::size_t>();
-        parser.add_argument("--size").help("Xephyr window size").default_value("1920x1080").metavar("SIZE");
+        parser.add_option("-x,--xephyr", rtn.xephyr, "Location of xephyr binary")->default_val("Xephyr");
+        parser.add_option("-a,--awesome", rtn.awesome, "Location of awesome binary")->default_val("awesome");
 
-        // Awesome Parameters
-        parser.add_argument("--config").help("AwesomeWM config to load").default_value("~/.config/awesome/rc.lua").metavar("PATH");
+        parser.add_option("-d,--display", rtn.display, "The Xorg display to use");
+        parser.add_option("-s,--size", rtn.size, "Size for the xephyr window")->default_val("1920x1080")->ignore_case();
 
-        // Live Reload
-        parser.add_argument("--reload").help("Enable auto-reload").default_value(true);
-        parser.add_argument("--recursive").help("Recursive Auto-Reload").default_value(true);
-        parser.add_argument("--watch").help("Directory to watch for live reload").nargs(0, 1).metavar("PATH");
+        parser.add_option("-r,--reload", rtn.reload, "Enable/Disable auto-reload")->default_val(true);
+        parser.add_option("-R,--recursive", rtn.recursive, "Watch files recursively")->default_val(true);
+
+        std::string config;
+        parser.add_option("-c,--config", config, "AwesomeWM config to load")->default_val("~/.config/awesome/rc.lua");
+
+        std::optional<std::string> watch;
+        parser.add_option("-w,--watch", watch, "Directory to watch for auto-reload");
+
+        parser.add_option("--awesome-args", rtn.awesome_args, "Additional arguments for awesome")->delimiter(',');
+        parser.add_option("--xephyr-args", rtn.xephyr_args, "Arguments for xephyr")->delimiter(',')->default_val(std::vector<std::string>{"-ac", "-br", "-noreset"});
 
         try
         {
-            parser.parse_args(argc, args);
+            parser.parse(argc, args);
         }
-        catch (const std::runtime_error &err)
+        catch (const CLI::ParseError &e)
         {
-            std::cout << parser << std::endl;
+            parser.exit(e);
             return std::nullopt;
         }
 
         std::regex validator{R"re(\d+x\d+)re"};
 
-        if (!std::regex_match(parser.get("--size"), validator))
+        if (!std::regex_match(rtn.size, validator))
         {
-            logger::get()->error("Invalid size: {}", parser.get("--size"));
-            std::cout << parser << std::endl;
+            logger::get()->error("Invalid size: {}", rtn.size);
+            std::cout << parser.help() << std::endl;
             return std::nullopt;
         }
 
-        return parser;
+        rtn.config = parse_path(config);
+
+        if (!std::filesystem::exists(rtn.config))
+        {
+            awmtt::logger::get()->error("Config '{}' doesn't exist", rtn.config.string());
+            return std::nullopt;
+        }
+
+        if (watch.has_value())
+        {
+            rtn.watch = parse_path(watch.value());
+        }
+        else
+        {
+            rtn.watch = rtn.config.parent_path();
+        }
+
+        if (!std::filesystem::exists(rtn.watch))
+        {
+            awmtt::logger::get()->warn("Watch directory '{}' doesn't exist", rtn.watch.string());
+            rtn.reload = false;
+        }
+
+        return rtn;
     }
 } // namespace awmtt
