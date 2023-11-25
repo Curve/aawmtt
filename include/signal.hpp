8cc94971eaea809033fbc8cc0c11cc378436524a
Curve <fynnbwdt@gmail.com>
2023-11-26 00:00:52 +0100

    refactor(sighandler): rename to `signal.hpp`

--- include/signal.hpp
+++ include/signal.hpp
@@ -1,20 +1,26 @@
 #pragma once
-#include "sighandler.hpp"
 
 #include <csignal>
 
-namespace awmtt::signals
+#include <utility>
+#include <concepts>
+
+namespace awmtt::signal
 {
-    template <callable T> void setup(int signal, T &&callback)
+    template <std::invocable T>
+    void setup(int signal, T &&callback)
     {
         static auto g_callback = std::forward<T>(callback);
         struct sigaction action;
 
-        action.sa_handler = [](int) { g_callback(); };
+        action.sa_handler = [](int)
+        {
+            g_callback();
+        };
 
         sigemptyset(&action.sa_mask);
         action.sa_flags = 0;
 
         sigaction(signal, &action, nullptr);
     }
-} // namespace awmtt::signals
+} // namespace awmtt::signal
